63369aa7ea28e94f6937a3347f8f7578d3aed021
P4 <P4@DESKTOP-3F0FNTI>
2021-10-29 14:18:24 -0400

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(40, 19);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(70, 25);
+            this.outputLabel.TabIndex = 7;
+            this.outputLabel.Text = "label1";
+            // 
+            // option1Button
+            // 
+            this.option1Button.BackColor = System.Drawing.Color.Black;
+            this.option1Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Button.ForeColor = System.Drawing.Color.White;
+            this.option1Button.Location = new System.Drawing.Point(45, 267);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(469, 46);
+            this.option1Button.TabIndex = 8;
+            this.option1Button.Text = "button1";
+            this.option1Button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.option1Button.UseVisualStyleBackColor = false;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.BackColor = System.Drawing.Color.Black;
+            this.option2Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Button.ForeColor = System.Drawing.Color.White;
+            this.option2Button.Location = new System.Drawing.Point(45, 340);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(469, 46);
+            this.option2Button.TabIndex = 9;
+            this.option2Button.Text = "button2";
+            this.option2Button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.option2Button.UseVisualStyleBackColor = false;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::AdventureGame.Properties.Resources.house;
+            this.pictureBox3.Location = new System.Drawing.Point(45, 57);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(469, 187);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(558, 398);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            option1Button.Text = "Enter the house.";
+            option2Button.Text = "Leave.";
+            outputLabel.Text = "You come across a creepy house.";
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1) { page = 2; }
+            else if (page == 2) { page = 3; }
+            else if (page == 3) { page = 4; }
+            else if (page == 4) { page = 5; }
+            else if (page == 5) { page = 1; } 
+            else if (page == 6) { page = 7; }
+            else if (page == 7) { page = 8; }
+            else if (page == 8) { page = 9; }
+            else if (page == 9) { page = 10; }
+            else if (page == 10) { page = 11; }
+            else if (page == 11) { page = 12; }
+
+            switch (page)
+
+            {
+                case 1:
+                    option1Button.Text = "Enter the house.";
+                    option2Button.Text = "Leave.";
+                    outputLabel.Text = "You come across a creepy house";
+                    pictureBox3.Image = Properties.Resources.house;
+                    break;
+                case 2:
+                    option1Button.Text = "Go down the hallway.";
+                    option2Button.Text = "Go up the staricase.";
+                    outputLabel.Text = "You see a hallway and a staircase.";
+                    pictureBox3.Image = Properties.Resources.interior;
+                    break;
+                case 3:
+                    option1Button.Text = "Keep going.";
+                    option2Button.Text = "Check it out.";
+                    outputLabel.Text = "You hear something go upstairs.";
+                    pictureBox3.Image = Properties.Resources.hallway;
+                    break;
+                case 4:
+                    option1Button.Text = "Open the stove.";
+                    option2Button.Text = "Keep going.";
+                    outputLabel.Text = "You enter the kitchen.";
+                    pictureBox3.Image = Properties.Resources.kitchen;
+                    break;
+                case 5:
+                    option1Button.Text = "Play again.";
+                    option2Button.Text = "Exit.";
+                    outputLabel.Text = "You got pushed into the stove and burned to death.";
+                    pictureBox3.Image = Properties.Resources.stove;
+                    break;
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+
+            if (page == 1) { page = 2; }
+            else if (page == 2) { page = 3; }
+            else if (page == 3) { page = 4; }
+            else if (page == 4) { page = 5; }
+            else if (page == 5) { page = 1; }
+            else if (page == 6) { page = 7; }
+            else if (page == 7) { page = 8; }
+            else if (page == 8) { page = 9; }
+            else if (page == 9) { page = 10; }
+            else if (page == 10) { page = 11; }
+            else if (page == 11) { page = 12; }
+
+            switch (page)
+
+            {
+                case 1:
+                    option1Button.Text = "Enter the house.";
+                    option2Button.Text = "Leave.";
+                    outputLabel.Text = "You come across a creepy house";
+                    pictureBox3.Image = Properties.Resources.house;
+                    break;
+                case 2:
+                    option1Button.Text = "Play Again.";
+                    option2Button.Text = "Exit.";
+                    outputLabel.Text = "You left and went home.";
+                    pictureBox3.Image = Properties.Resources.interior;
+                    break;
+                case 3:
+                    Application.Exit();
+                    break;
+                case 4:
+                    option1Button.Text = "You enter the living room. Someone is on the couch.";
+                    option2Button.Text = "Talk to them.";
+                    outputLabel.Text = "Sneak past them.";
+                    pictureBox3.Image = Properties.Resources.livingroom;
+
+                    break;
+                case 5:
+                    option1Button.Text = "";
+                    option2Button.Text = "";
+                    outputLabel.Text = "";
+                    break;
+
+
+
+            }
+        }
+    }
+}
+
